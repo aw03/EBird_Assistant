a7cfa96988631ea88a7083d9585b0bb4977596bf
aw03 <awu245@gmail.com>
2024-11-30 21:29:47 -0500

    fixed bugs

--- pages/2_Find_Hotspots.py
+++ pages/2_Find_Hotspots.py
@@ -3,8 +3,13 @@ import pandas as pd
 import ebird_analyze
 from region import Region 
 
+desired_columns = ["locName","lat","lng","latestObsDt","numSpeciesAllTime"]
+
+st.title("Find Hotspots!")
+
 if "region" not in st.session_state or str(st.session_state['region']) == "World":
     st.warning("Please Select a Region")
 else:
-    st.text(st.session_state['region'])
-    st.dataframe(ebird_analyze.region_hotspots_dataframe(repr(st.session_state['region']))[["locName","lat","lng","latestObsDt","numSpeciesAllTime"]])
+    st.header(st.session_state['region'])
+    st.dataframe(ebird_analyze.region_hotspots_dataframe(repr(st.session_state['region'])).reindex(columns=desired_columns))
+    
