7ab1a7b7cb4ace20982d583ed58b71b7be907d89
aw03 <awu245@gmail.com>
2024-11-30 21:08:39 -0500

    Working Hotspot Finds

--- pages/2_Find_Hotspots.py
+++ pages/2_Find_Hotspots.py
@@ -1,6 +1,10 @@
 import streamlit as st
+import pandas as pd
+import ebird_analyze
+from region import Region 
 
-if "region" not in st.session_state and str(st.session_state['region']) != "World":
+if "region" not in st.session_state or str(st.session_state['region']) == "World":
     st.warning("Please Select a Region")
 else:
-    pass
+    st.text(st.session_state['region'])
+    st.dataframe(ebird_analyze.region_hotspots_dataframe(repr(st.session_state['region']))[["locName","lat","lng","latestObsDt","numSpeciesAllTime"]])
