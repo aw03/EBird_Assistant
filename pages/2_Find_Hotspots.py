5eee0ac693a2a4cac0cdf4aee6f9018fc9407ca9
aw03 <awu245@gmail.com>
2025-01-16 10:37:14 -0300

    select hotot

--- pages/2_Find_Hotspots.py
+++ pages/2_Find_Hotspots.py
@@ -11,5 +11,10 @@ if "region" not in st.session_state or str(st.session_state['region']) == "World
     st.warning("Please Select a Region")
 else:
     st.header(st.session_state['region'])
-    st.dataframe(ebird_analyze.region_hotspots_dataframe(repr(st.session_state['region'])).reindex(columns=desired_columns))
+    hotspots = ebird_analyze.region_hotspots_dataframe(repr(st.session_state['region'])).reindex(columns=desired_columns)
+    st.dataframe(hotspots)
+    
+    selected_hotspot_name = st.selectbox("Select a Hotspot", hotspots['locName'])
+    # hot_spot_map = pd.
+    # st.map()
     
